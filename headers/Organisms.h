be30025dfbb54ea481952486890ccc659db286b5
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-05-02 23:35:37 +0200

    Add header fille for all organisms

---
+++ headers/Organisms.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Organism.h"
+#include "Plant.h"
+#include "Animal.h"
+#include "Wolf.h"
+#include "Sheep.h"
+#include "Grass.h"
+#include "Dandelion.h"
+#include "ToadStool.h"
