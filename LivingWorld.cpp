0166494060e8da5898b590582736f9342bd49871
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-05-03 17:11:10 +0200

    Refactored example world

---
+++ LivingWorld.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "headers/Position.h"
+#include "headers/World.h"
+#include "headers/Organisms.h"
+
+int main()
+{
+	// Random generator initialization
+	srand(time(0));
+
+	// Creating world object
+	World* world = new World(8, 8);
+
+	// Creating some organisms living in this worldd
+	world->addOrganism(new Wolf(Position(0, 0)));
+	world->addOrganism(new Wolf(Position(0, 3)));
+	world->addOrganism(new Wolf(Position(5, 0)));
+	world->addOrganism(new Sheep(Position(6, 6)));
+	world->addOrganism(new ToadStool(Position(3, 2)));
+	world->addOrganism(new Dandelion(Position(4, 2)));
+
+	// Printing out world
+	std::cout<<world->toString()<<std::endl;
+
+	// Printing world after some turns
+	for(int i = 0; i<15; i++) world->makeTurn();
+	std::cout<<world->toString()<<std::endl;
+
+	auto anc = world->getOrganisms().at(0)->getAncestors();
+	for(auto organism : world->getOrganisms()){
+		for (auto& ancestor : organism->getAncestors()){
+			std::cout<<"B:"<<ancestor.birthTurn<<" "<<"D:"<<ancestor.deathTurn<<std::endl;
+		}
+		std::cout << std::endl;
+	}
+
+	// Saving world to the file
+	world->writeWorld("save");
+	delete world;
+
+	// Loading saved world
+	World* newWorld = new World(6, 6);
+	newWorld->readWorld("save");
+	std::cout<<newWorld->toString()<<std::endl;
+
+	// Run simulation of world
+	// newWorld->run();
+
+	return 0;
+}
