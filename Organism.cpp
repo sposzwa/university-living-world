c2a9710918bef5a94ad45bd3861f7de23ae2305c
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-05-11 00:37:28 +0200

    Modified copy constructor

--- Organism.cpp
+++ Organism.cpp
@@ -9,136 +9,146 @@ Organism::Organism(int power, int initiative, int liveLength, int powerToReprodu
 Organism::Organism(Position pos, int power, int initiative, int liveLength, int powerToReproduce, std::string sign) 
 : power(power), initiative(initiative), position(pos), liveLength(liveLength), powerToReproduce(powerToReproduce), sign(sign) {};
 
-Organism::~Organism(){
-	if(world!=nullptr){
+Organism::~Organism() {
+	if(world!=nullptr) {
 		Notify();
 		world->removeOrganism(this);
 	}
 };
 
-void Organism::operator=(const Organism& organism){
+void Organism::operator=(const Organism& organism) {
 	power = organism.power;
 	initiative = organism.initiative;
+	position = organism.position;
 	liveLength = organism.liveLength;
 	powerToReproduce = organism.powerToReproduce;
 }
 
 Organism::Organism(Organism const& organism)
-: Organism(organism.power, organism.initiative, organism.liveLength, organism.powerToReproduce, organism.sign) {};
+: Organism(organism.position, organism.power, organism.initiative, organism.liveLength, organism.powerToReproduce, organism.sign) {
+	// cout for testing copy constructor in LivingWorld
+	std::cout << std::endl <<  "COPYING ORGANISM: " << organism.sign << ", " << organism.position << std::endl;
+	for (auto& anc : organism.ancestors)
+		std::cout << "Birth: " << anc.birthTurn << ", Death: " << anc.deathTurn << std::endl;
+	std::cout << std::endl;
 
-std::string Organism::toString(){
+	world = organism.world;
+	ancestors = organism.ancestors;
+	descendants = organism.descendants;
+};
+
+std::string Organism::toString() {
 	return "{ sign: " + this->getSign() +  
 		", position: " + getPosition().toString() + "}";
 }
 
-int Organism::getPower(){
+int Organism::getPower() {
 	return power;
 }
 
-void Organism::setPower(int power){
+void Organism::setPower(int power) {
 	this->power = power;
 }
 
-int Organism::getInitiative(){
+int Organism::getInitiative() {
 	return initiative;
 }
 
-void Organism::setInitiative(int initiative){
+void Organism::setInitiative(int initiative) {
 	this->initiative = initiative;
 }
 
-Position Organism::getPosition(){
+Position Organism::getPosition() {
 	return position;
 }
 
-void Organism::setPosition(Position position){
+void Organism::setPosition(Position position) {
 	this->position = position;
 }
 
-int Organism::getLiveLength(){
+int Organism::getLiveLength() {
 	return this->liveLength;
 }
 
-void Organism::setLiveLength(int liveLength){
+void Organism::setLiveLength(int liveLength) {
 	this->liveLength = liveLength;
 }
 
-int Organism::getPowerToReproduce(){
+int Organism::getPowerToReproduce() {
 	return this->powerToReproduce;
 }
 
-void Organism::setPowerToReproduce(int powerToReproduce){
+void Organism::setPowerToReproduce(int powerToReproduce) {
 	this->powerToReproduce = powerToReproduce;
 }
 
-std::string Organism::getSign(){
+std::string Organism::getSign() {
 	return this->sign;
 }
 
-void Organism::setSign(std::string sign){
+void Organism::setSign(std::string sign) {
 	this->sign = sign;
 }
 
-int Organism::getTurnOfBirth(){
+int Organism::getTurnOfBirth() {
 	return turnOfBirth;
 }
 
-void Organism::setTurnOfBirth(int turn){
+void Organism::setTurnOfBirth(int turn) {
 	turnOfBirth = turn;
 }
 
-World* Organism::getWorld(){
+World* Organism::getWorld() {
 	return world;
 }
 
-void Organism::setWorld(World* newWorld){
+void Organism::setWorld(World* newWorld) {
 	world = newWorld; 
 }
 
-std::vector<Ancestor> Organism::getAncestors(){
+std::vector<Ancestor> Organism::getAncestors() {
 	return ancestors;
 }
 
-void Organism::setAncestors(std::vector<Ancestor> anc){
+void Organism::setAncestors(std::vector<Ancestor> anc) {
 	ancestors = anc;
 }
 
-std::vector<IObserver*> Organism::getDescendants()
-{
+std::vector<IObserver*> Organism::getDescendants() {
 	return descendants;
 }
 
-void Organism::setDescendants(std::vector<IObserver*> desc){
+void Organism::setDescendants(std::vector<IObserver*> desc) {
 	descendants = desc;
 }
 
-bool Organism::canReproduce(){
+bool Organism::canReproduce() {
 	return power > powerToReproduce;
 }
 
-std::optional<Position> Organism::getPositionForReproduction(){
+std::optional<Position> Organism::getPositionForReproduction() {
 	auto freePositionAround = world->getVectorOfFreePositionsAround(position);
 	if(freePositionAround.empty()) return std::nullopt;
 	int randIndex = rand() % freePositionAround.size();
 	return freePositionAround[randIndex];
 }
 
-std::optional<Position> Organism::getPositionForMove(){
+std::optional<Position> Organism::getPositionForMove() {
 	auto validPostionAround = world->getVectorOfValidMovePosition(position);
 	if(validPostionAround.empty()) return std::nullopt;
 	int randIndex = rand() % validPostionAround.size();
 	return validPostionAround[randIndex];
 }
 
-void Organism::Attach(IObserver* observer){
+void Organism::Attach(IObserver* observer) {
 	descendants.push_back(observer);
 }
 
-void Organism::Detach(IObserver* observer){
+void Organism::Detach(IObserver* observer) {
 	descendants.erase(std::remove(descendants.begin(), descendants.end(), observer), descendants.end());
 }
 
-void Organism::Notify(){
+void Organism::Notify() {
 	for (auto& descendant : descendants) {
 		descendant->Update(this);
 	}
@@ -148,7 +158,7 @@ void Organism::Notify(){
 	}
 }
 
-void Organism::Update(ISubject* subject){ 
+void Organism::Update(ISubject* subject) { 
 	for (auto& ancestor : ancestors) {
 		if (ancestor.subject == subject) {
 			ancestor.deathTurn = dynamic_cast<Organism*>(ancestor.subject)->getWorld()->getTurn();
